ca066cbd5a1ed87dbf3b082a5b2f02ea5a15ea86
gregglegarda <gregglegarda@gwu.edu>
2021-03-11 09:57:22 -0500

    added element data

--- js/display.js
+++ js/display.js
@@ -124,7 +124,7 @@ var layout = {
       showarrow: false,
       text: 'Daily Average',
       x: 0.5,
-      y: 0.21
+      y: 0.22
     },
     {
       font: {
@@ -133,7 +133,7 @@ var layout = {
       showarrow: false,
       text: 'Today\'s Clicks',
       x: 0.5,
-      y: 0.79
+      y: 0.78
     }
   ],
   height: 600,
