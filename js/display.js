c8703c383c213e872a8bfac81f64301dda209a9c
gregglegarda <gregglegarda@gwu.edu>
2021-03-11 00:07:58 -0500

    update with display.js

--- js/display.js
+++ js/display.js
@@ -30,7 +30,7 @@ function create_trace(nameOfElement, countOfElement){
 	  autobinx: false, 
 	  histnorm: "count", 
 	  marker: {
-		color: "rgba(255, 100, 102, 0.7)", 
+		color: "rgba(255,100,102,0.7)",
 		 line: {
 		  color:  "rgba(255, 100, 102, 1)", 
 		  width: 1
@@ -79,104 +79,9 @@ function plot_histogram(){
 	  xaxis: {title: "Element"}, 
 	  yaxis: {title: "Count"}
 	};
-	var config = {responsive: true}
+	var config = {responsive: true};
 	Plotly.newPlot('myDiv', data, layout, config);
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function create_trace2(x, y){
-	var x1 = x;
-	var y1 = y;
-
-	var name = x;
-	
-
-	console.log(x1);
-	console.log(y1);
-	
-	var trace1 = {
-	  x: x1,
-	  y: y1,
-	  name: name,
-	  autobinx: false, 
-	  histnorm: "count", 
-	  marker: {
-		color: "rgba(255, 100, 102, 0.7)", 
-		 line: {
-		  color:  "rgba(255, 100, 102, 1)", 
-		  width: 1
-		}
-	  },  
-	  opacity: 0.5, 
-	  type: "bar", 
-	  xbins: {
-		end: 14, 
-		size: 0.22, 
-		start: -22
-	  }
-	};
-	
-	return trace1;
-	
-}
-
-
-
-
-
-
-
-
-function push_trace_to_array2(){
-/************************ push the trace to an array to be plotted later *************************/
-	var data = [];
-	var x=[];
-	var y=[];
-	for (var obj of arrayOfElementCounts) {
-		
-			for (const key in obj){
-				x.push(key);
-				y.push(obj[key]);
-				
-				
-				//push each trace into the traceDataArray
-				
-			}
-	}
-	var sampleTrace = create_trace2(x, y);
-	data.push(sampleTrace);
-	return data;
-}
-
-
-function plot_bar(){
-	var data = push_trace_to_array2();
-	var layout = {
-	  bargap: 0.05, 
-	  bargroupgap: 0.2, 
-	  barmode: "overlay", 
-	  title: "Element Counts", 
-	  xaxis: {title: "Element"}, 
-	  yaxis: {title: "Count"}
-	};
-	var config = {responsive: true}
-	Plotly.newPlot('myDiv2', data, layout, config);
-}
