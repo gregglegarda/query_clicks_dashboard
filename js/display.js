8d6c8d24278daf5b4acb7595bbe9ff17841f8c61
gregglegarda <gregglegarda@gwu.edu>
2021-03-11 11:08:29 -0500

    total clicks to string

--- js/display.js
+++ js/display.js
@@ -4,9 +4,9 @@ function display_numbers_on_front(){
 
 		/********** Connect Global Value ***********/
 	// connects the global variables to the front end
-	console.log("Total Clicks: " + totalNumberOfClicks);
-	$("#total").text("Total Clicks: " + totalNumberOfClicks);
-	$("#avgclicks").text("Average Daily Clicks: " +avgNumberOfClicks);
+	console.log("Total Clicks: " + typeof totalNumberOfClicks);
+	$("#total").text("Total Clicks: " + totalNumberOfClicks.toString());
+	$("#avgclicks").text("Average Daily Clicks: " +avgNumberOfClicks.toString());
 
 
 
