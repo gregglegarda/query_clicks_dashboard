4568d2dc42eeca7e5b65992c1c59c18c8e71d16d
gregglegarda <gregglegarda@gwu.edu>
2021-03-12 16:24:50 -0500

    organized folders

---
+++ display/donut.js
@@ -0,0 +1,47 @@
+console.log("DONUT FUNCTIONS");
+
+
+
+
+/*******************************************************************************************
+										Plotly DONUT Functions
+*******************************************************************************************/
+
+
+
+
+function plot_donut(){
+var data = [{
+  values: [16, 15, 12, 6, 5, 4, 42],
+  labels: elementArrayNames,
+  domain: {row: 0},
+  name: 'GHG Emissions',
+  hoverinfo: 'label+percent+name',
+  hole: .4,
+  type: 'pie'
+
+}];
+
+var layout = {
+  title: 'Element Click Counts',
+  annotations: [
+    {
+      font: {
+        size: 10
+      },
+      showarrow: false,
+      text: 'Daily Average',
+      x: 0.5,
+      y: 0.5
+    }
+  ],
+  height: 600,
+  width: 400,
+  showlegend: false,
+  grid: {rows: 1, columns: 1}
+};
+var config = {responsive: true};
+Plotly.newPlot('myDivDonut', data, layout,config);
+
+}
+
